dc1567efff62ed00fffe7f1ab97003d31ab18360
Marie Papa <Marie Papa>
2017-04-01 14:58:33 -0700

    wtf internet

--- Casual.py
+++ Casual.py
@@ -8,6 +8,19 @@ def STAddress(string, *args):
     return "not done"
 
 def STVision(Addresslist):
+    '''
+    note: this func is expecting aa # from 1>2^some N
+    addresslist:
+    [basis, L value, number]
+    take addresslist and return ??? [fk I can only return algorithms]
+    [it's ok because you have an ordered basis and dealing with directed graphs so you just use lexicographic ordering to produce algorithm for everything]
+    '''
+    #do PBL?
+    #check if error (?)(?)(?)(?)
+        #tobinary
+        #return the algorithm
+    #problem: how to do L>2
+    
     return "not done"
 
 
