6c47baa84430ffbb57df7c09fa5374939b7666ec
RPC <groditz@gmail.com>
2017-05-28 15:18:40 -0700

    need to reconcile cantor and binary mapping + why does there exist such a dumb compression when adding [ and ] to a basis

--- Delta Func/autocomposer.py
+++ Delta Func/autocomposer.py
@@ -83,11 +83,14 @@ from math import *
         
 
 
-def AutoVision(int,Lval):
+def AutoVision(number,Lval):
+    '''
+    this is for 
+    '''
     ANS = []
-    binary = "{0:b}".format(int)[::-1]
+    binary = "{0:b}".format(number)[::-1]
     if Lval == 1:
-        z = int
+        z = number
         w = floor((sqrt(8*z+1)-1)/2)
         t = (w*w+w)/2
         y = z - t
@@ -132,8 +135,8 @@ def gcomposition(graph1,graph2):
         #print(x[1],Eps(x[1],g2dom))
         #if Eps(x[1],g2dom) > -1:
         for y in graph2:
-            print("x,y", x,y)
-            print(x,"wtf",y)
+            #print("x,y", x,y)
+            #print(x,"wtf",y)
             #print(y[0])
             #print(x[1])
             yfail = 0
@@ -165,10 +168,225 @@ final = 5
 for top in range(0,final):
     for x in range(0,top+1):
         for y in range(0,top+1):
-            print(x,y,top)
-            print("input first:" ,AutoVision(x,top),AutoVision(y,top))
+            #print(x,y,top)
+            #print("input first:" ,AutoVision(x,top),AutoVision(y,top))
             
             file.write(str([[AutoVision(x,top),AutoVision(y,top)],gcomposition(AutoVision(x,top),AutoVision(y,top))]))
     
 ###this has a problem with objects that aren't in there:: "IndexError: list index out of range"
 file.close()
+
+'''
+working on:
+address:
+idea:
+facts:
+have string
+want: address which is a specific number when assigned a basis:
+need a basis:
+
+
+
+have:
+string to address
+basis
+pairing chars
+
+job #1: figure out L-vals
+idea: I think I build the address from the "bottom up" right?
+function
+    if l-val not 1
+    call itself with lval-1
+    then append to ans
+
+
+
+example:
+string = test
+
+#1: check basis (all the way down to the "bottom")
+#2: go down levels until you can do L=1:
+#3: how to put it all together?
+
+
+F U C K there is like a compression theorem here somewhere
+thm ==  everything can be set to L value of 1???
+
+>so wtf if you add the pair chars like everything is expressible as long as you have a long enough (enough integers) basis and have the right chars
+picture:
+limited/small basis: [ALPHA OPTION]
+requres a few powersets to get to an object
+
+huge basis + special chars: [BETA OPTION]
+can express a lot of shit and is a "wider" catch
+also reduces the distances to theorems significantly
+
+
+new question: even if you can express it, can you evaluate it properly?
+[ALPHA:]
+yes
+[BETA:]
+no, too "limited" in a sense if I only use gcomposition
+
+example:
+L == 2
+[[[],[]],[]] <-- I can get this from the cheat() function
+string>list
+for each element in list(string):
+    if every element is in basis:
+        assign a number then return answer
+    else:
+        call this function again with one less l-val on each element of element in list
+
+
+
+
+
+for each element in list(string):
+    if we can't go down "further"
+    if element[0] fails:
+        address the objects here
+    else:
+        call this function again and address with one less lval
+         
+
+
+goal: string + basis > output a unique integer
+problem: when string in higher powersets of the basis:
+example:
+basis = c,a,t
+BUT I have
+"cat"
+    
+
+'''
+
+#insert into a list at the right index
+def InsertAt (List,obj,Index):
+    '''
+    Inserts obj at List[Index] and appends the rest of list after it
+    '''
+    VALUE = List[:Index]
+    VALUE.append(obj)
+    for x in range(0,len(List[Index:])):
+        VALUE.append(List[Index + x])
+    return VALUE
+
+def pairfinder(string,charpair):
+    #delete pairs:
+    thefuckinganswer = []
+    i = 0
+    index = 0
+    maxi = 0
+    for x in string:
+        index += 1
+        if x == charpair[0]:
+            i+= 1
+            thefuckinganswer.append([index,i])
+            if i > maxi:
+                maxi = i
+        if x == charpair[1]:
+            i= i - 1
+            thefuckinganswer.append([index,i])
+    return [thefuckinganswer,maxi]
+
+
+'''
+FUCK THIS MIGHT BE WRONG BECAUSE IT DOESN"T GIVE METATHEOREMS????
+MIGHT NEED ANOTHER FUNC FOR THAT
+'''
+#note: this has no cantor pairings
+def Addresspls(info):
+    #info = [string,basis,pairchars,Lval]
+    #NOTE: WE ARE GIVEN LVAL
+    ANS = []
+    #check if every element is in basis:
+    basisyes = 1
+    for x in list(info[0]):
+        try:
+            info[1].index(x)
+        except(ValueError,IndexError):
+            basisyes = 0
+    #now we try to assign values
+    #interim tells you which parts of a binary number is 1
+    Interim = []
+    for x in list(info[0]):
+        if basisyes == 1:
+            #assign a number then return answer
+            #assign a number:
+            Interim.append(info[1].index(x))
+        else:
+            #call this function again with one less l-val on each element of element in list
+            Addresspls([x,info[1],info[2],info[3]-1])
+    #change interim into actual binary:
+    #findmaxlength:
+    longest = 0
+    for x in Interim:
+        if x > longest:
+            longest = x
+    #init binary:
+    
+    ANS="1".zfill(int(longest)+1)
+    Interim = list(filter(lambda x: x != longest, Interim))
+    for x in Interim:
+        ANS = ANS[:x] + "1" + ANS[x+1:]
+    ANS = int(ANS, 2)
+    return ANS
+
+
+
+#now to modify AutoVision to work with an arbitrary basis:
+
+def Vision(number,basis,Lval):
+    '''
+    this is for 
+    '''
+    ANS = ""
+    binary = "{0:b}".format(number)[::-1]
+    if Lval == 1:
+        indexer = 0
+        for x in binary:
+            if x == "1":
+                try:
+                    basis[indexer]
+                except(ValueError,IndexError):
+                    print("FML")
+            ANS = ANS + basis[indexer]
+            indexer +=1
+    else:
+        '''
+        for each one, construct the part recursively:
+	go down 1 Lval
+	'''	
+        i = 0
+        for x in binary:
+            if x == "1":
+                '''
+                what do we do?
+                L-1
+                because L == 1 we eval and append
+                '''
+                ANS.append(AutoVision(i,basis,Lval-1))
+            i += 1
+    return ANS
+
+#print("checking addresspls")
+compress = Addresspls(["cat",["c","a","t"],[],pairfinder("cat",["[","]"])[1]])
+print(compress)
+#print("checking vision",AutoVision(7,pairfinder("cat",["[","]"])[1]))
+print(Vision(compress,["c","a","t"],1))
+
+#problem: we need to be given an L-val first instead of figuring it out beforehand... :/
+def AddressplsWRONG(info):
+    #info = [string,basis,pairchars,Lval]
+    ANS = []
+    #1: figure out L-val if L-val is missing:
+    try:
+        info[3]
+    except(ValueError,IndexError):
+        info[3] = pairfinder(info[0])[1]
+    if info[3] != 1:
+        #1: you need to get the right string to input into nested addresspls:
+        Addresspls([info[0],info[1],info[2],info[3]-1])
+    return ANS
+
--- Delta Func/powerset testing.py
+++ Delta Func/powerset testing.py
@@ -5,11 +5,12 @@ def powerset(iterable):
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 #for x in list(powerset(list(powerset([[0,0],[0,1],[1,0],[2,0]])))):
-for x in list(powerset("12")):
-    print(x)
+#for x in list(powerset("12")):
+#    print(x)
 
 #print(list(powerset([[0,0],[1,0],[0,1][1,1]])))
 
 #for x in range(0,16):
 #    print("{0:b}".format(x).zfill(4)[::-1])
 
+print(["C","Z","Y","C","R"].index("C"))
