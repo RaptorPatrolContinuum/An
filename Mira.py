28063d1188e424171b62ddaace2eb321eb839fa7
RPC <groditz@gmail.com>
2017-08-18 16:15:58 -0700

    preparation of rites

--- Mira.py
+++ Mira.py
@@ -1,9 +1,75 @@
 from math import *
+from inspect import *
 import ast
 
 file = open('INP.txt', 'r')
 basis = open('Basis.txt', 'r+')
 memory = open('Memory.txt', 'r+')
+
+def ARB(function, replacelist):
+    '''
+    god fucking damn it I hate writing one function I just want to abstract as fast as possible
+
+    FML IF I HAD A CONVERTER TO CONCEPT SPACE STRUCTURE I COULD DO THIS ALMOST INSTANTLY
+
+    example:
+    for x in alg1:
+        f1.append(x[1])
+
+    use replacelist:
+    alg1:alg2
+    f1:f2
+
+    then eval:
+    for x in alg2:
+        f2.append(x[1])
+    problem:
+         sometimes it replaces the wrong thing!
+
+    EX:
+    for r in alg1:
+        g1.append(r[1])
+
+    with:
+    r:x
+    g1:f1
+
+    I would get:
+    fox x in alf1:
+        f1.append(x[1])
+
+    idea:
+         use syntax hax (I need to keep a fucking library god damn it)
+         know what is a var and what isn't a var
+
+    idea: use positionals (would be too complicated?)
+    
+    '''
+    NEWEVAL = []
+    return NEWEVAL
+
+def SynSplit(string):
+    '''
+    function so I can make ABStractreplace better
+    idea: feed lines in
+    split each line:
+
+    EX:
+    for r in alg1:
+        g1.append(r[1])
+
+    with:
+    r:x
+    g1:f1
+
+    try:
+    for r in alg1:
+
+    split into:
+    [for| |r| |in| |alg1:]
+         [g1|.append(| r | [|1|] |)]
+    '''
+    return
 
 def M_(thestr):
     ANS = []
@@ -12,6 +78,9 @@ def M_(thestr):
         ANS.append([str(i),x])
         i += 1
     return ANS
+
+def Q_(unknown):
+    return [[unknown,unknown]]
 
 def CantorPair(x,y):
     return (1/2)*(x+y)*(x+y+1)+y
@@ -22,6 +91,18 @@ def CantorInv(z):
     y = z - t
     x = w - y
     return [x,y]
+
+def fCheck(fcandidate):
+    ANS = True
+    '''
+    an object is not a function when:
+    one element of the fcandidate is not a pair
+    check if each element of fcandidate is of size 2
+    '''
+    for obj in fcandidate:
+        if len(obj) != 2:
+            ANS = False
+    return ANS
 
 def Address(basis,obj):
     '''
@@ -59,42 +140,216 @@ def AutoVision(number,Lval):
         indexer = 0
         for d in str(binary):
             if d == '1':
-                print("what is index?", indexer, CantorInv(indexer))
+                #print("what is index?", indexer, CantorInv(indexer))
+                ANS.append(CantorInv(indexer))
             indexer += 1
-        '''
-        z = number
-        w = floor((sqrt(8*z+1)-1)/2)
-        t = (w*w+w)/2
-        y = z - t
-        x = w - y
-        
-        #print("number, append", number, [x,y])
-        #print("how the fuck do you even get empty pair", number,[x,y])
-        ANS.append([x,y])
-        '''
     else:
         '''
         for each one, construct the part recursively:
 	go down 1 Lval
 	'''	
-        i = 0
-        for x in binary:
-            if x == "1":
-                '''
-                what do we do?
-                L-1
-                because L == 1 we eval and append
-                '''
-                ANS.append(AutoVision(i,Lval-1))
-            i += 1
-        #FUCKING ZERO ALWAYS IS AN EXCEPTION THE DAMN BASTARD
-        if number == 0:
-            #ANS.append(AutoVision(0,Lval-1))
-            ANS.append([[0.0, 0.0]])
+        print("ABUSE THE COMPRESSION THEOREM!")
     if ANS == []:
         #print("stats", number,Lval)
-        print("ANS IS []!")
+        print("ANS IS []???!?!?!?!?!")
     return ANS
+
+def Compose(f1,f2):
+    '''
+    do f2 THEN f1 like the way it's written!
+    '''
+    #check if f1,f2 are functions:
+    if fCheck(f1) == False or fCheck(f2) == False:
+        print("f1 is function?", fCheck(f1), "f2 is function?", fCheck(f2))
+        return
+    ALG = []
+    for x in f2:
+        for y in f1:
+            if x[1] == y[0]:
+                ALG.append([x[0],y[1]])
+    return ALG
+
+def M_compose(alg1, alg2):
+    '''
+    do ALG2 THEN ALG1!!!!
+    problem: what if alg2 or alg1 don't map to functions???
+    say that alg1 or alg2 are not mapped to funcs
+    alg1 and alg2 should be inspectors of functions....
+    '''
+    ALG = []
+    f1 = []
+    f2 = []
+    #construct f1 and f2
+    for x in alg1:
+        f1.append(x[1])
+    #check if they are functions
+    
+    #compose them
+    
+    return ALG
+
+def pairfinder(string,charpair):
+    #delete pairs:
+    thefuckinganswer = []
+    i = 1
+    #we start at 1 because we are already using for loop AKA for loop assumes we already have 1 level
+    index = 0
+    maxi = 0
+    for x in string:
+        if x == charpair[0]:
+            thefuckinganswer.append([index,i])
+            if i > maxi:
+                maxi = i
+            i+= 1
+        if x == charpair[1]:
+            i= i - 1
+            thefuckinganswer.append([index,i])
+        index += 1
+    return [thefuckinganswer,maxi]
+
+def pairfinderSTRING(finderlist,string):
+    '''
+    takes pairfinder and the original string and returns the right morphemes
+    '''
+    ANS = []
+    Indexers = {}
+    for y in range(1,finderlist[1]+1):
+        Indexers[y] = []
+    for x in finderlist[0]:
+        #print("flist", finderlist)
+        #print("cstats", x, Indexers[x[1]],x[0])
+        '''
+        why is Indexers[x[1]] have a keyerror wtf
+        '''
+        Indexers[x[1]].append(x[0])
+    for y in range(1,finderlist[1]+1):
+        for z in range(0,len(Indexers[y])):
+            if z % 2 == 0:
+                ANS.append(string[Indexers[y][z]:Indexers[y][z+1]+1])
+    return ANS
+
+def Cheat(string):
+    #take a string and return a list of strings that represent the important morphemes:
+    #punctuation:
+    #.?!,;:-—)}]'"...
+    #line break
+    #paragraph break(?)
+    
+    #FUCKING USE CASES FOR "'"
+    Morphemes = []
+    
+    sentencedelimiters = [".", "?", "!"] #...
+    SentenceStartPos = 0
+    SentenceStartPosList = []
+    #every even is start and odd is close for each pair
+    pairdelimiters=["(", ")", "{", "}", "[", "]", "\"", "\"", " ", " "]
+    pairdelimiterpos={}
+    #EACH PAIR CHAR NEEDS ITS OWN START POS
+    for pair in pairdelimiters:
+        #use dictionaries
+        pairdelimiterpos[pair + "On"]= 0
+        pairdelimiterpos[pair + "Location"]= [-1]
+        #starts at -1 so that it gets the first word even though there is no space[might be a problem]
+        pairdelimiterpos[pair + "List"]= []
+
+    pairdelimiterpos["AllList"] = [-1,-1]
+    #starts at -1 so that it gets the first word even though there is no space[might be a problem]
+
+    splicedelimiters=[",", ";", ":", "-", "—", "\n"]
+    SpliceStartPos = 0
+
+    for g in range(0,len(pairdelimiters)):
+        if g % 2 == 0 and pairdelimiters[g] != " ":
+            toappend = pairfinderSTRING(pairfinder(string,[pairdelimiters[g],pairdelimiters[g+1]]),string)
+            if len(toappend) != 0:
+                Morphemes.append(toappend)
+
+    
+    i = 0
+    for x in string:
+        if i == len(string)-1:
+            #check if last morpheme is already in the list:
+            inside = 0
+            try:
+                Morphemes.index(string[pairdelimiterpos["AllList"][-1]+1:])
+            except(ValueError,IndexError):
+                #print("already inside!","\n",string[pairdelimiterpos["AllList"][-1]+1:],"\n", Morphemes)
+                inside = 1
+            if inside == 0:
+                #get the last morpheme:
+                Morphemes.append(string[pairdelimiterpos["AllList"][-1]+1:])
+        #you need a hierarchy to get sentence start pos to ignore pairdelimiter rules
+        if x in splicedelimiters:
+            if max(SentenceStartPos,SpliceStartPos) in range(pairdelimiterpos["AllList"][-2],pairdelimiterpos["AllList"][-1]+1) and len(pairdelimiterpos["AllList"])>=4:
+                #find one that isn't in most recent pair
+                k = 1
+                for y in SentenceStartPosList:
+                    K = len(SentenceStartPosList)
+                    if SentenceStartPosList[K-k] not in range(pairdelimiterpos["AllList"][-2],pairdelimiterpos["AllList"][-1]+1):
+                        delimstart = SentenceStartPosList[K-k]
+                    else:
+                        delimstart = 0
+                    k += 1
+            else:
+                delimstart = max(SentenceStartPos,SpliceStartPos)
+            Morphemes.append(string[delimstart:i+1])
+            #keep track of everything
+            pairdelimiterpos["AllList"].append(i)
+            SpliceStartPos = i
+        if x in pairdelimiters:
+            '''
+            THEY ARE BEING TREATED AS A SINGLE INSTEAD OF AS A PAIR
+            '''
+            if x == " " or x == "\"":
+                #append the slice from the last location to this location
+                Morphemes.append(string[pairdelimiterpos[x + "Location"][-1]+1:i])
+                #make note of the last location
+                pairdelimiterpos[x + "Location"].append(i)
+                #keep track of everything
+                pairdelimiterpos["AllList"].append(i)
+            else:
+                #if it's on for this particular character, slice the string then turn off
+                if pairdelimiterpos[pairdelimiters[pairdelimiters.index(x)-1] + "On"] == 1:
+                    pairdelimiterpos[pairdelimiters[pairdelimiters.index(x)-1] + "On"] = 0
+                    #slice the string and append to Morphemes
+                    Morphemes.append(string[pairdelimiterpos[pairdelimiters[pairdelimiters.index(x)-1] + "Location"][-1]:i+1])
+                    #add to both location lists
+                    pairdelimiterpos[pairdelimiters[pairdelimiters.index(x)-1] + "Location"].append(i)
+                    #keep track of everything
+                    pairdelimiterpos["AllList"].append(i)
+                else:
+                    pairdelimiterpos[x + "Location"].append(i)
+                #be smart about how to turn this on:
+                if pairdelimiters.index(x)%2 == 0:
+                    pairdelimiterpos[x + "On"] = 1
+        if x in sentencedelimiters:
+            #know the last .?! , (if any) then cut between first and last .?!
+            #ignore pair delimiters
+            if SentenceStartPos in range(pairdelimiterpos["AllList"][-2],pairdelimiterpos["AllList"][-1]+1) and len(pairdelimiterpos["AllList"])>=4:
+                #find one that isn't in most recent pair
+                k = 1
+                for y in SentenceStartPosList:
+                    K = len(SentenceStartPosList)
+                    if SentenceStartPosList[K-k] not in range(pairdelimiterpos["AllList"][-2],pairdelimiterpos["AllList"][-1]+1):
+                        delimstart = SentenceStartPosList[K-k]
+                    else:
+                        delimstart = 0
+                    k += 1
+            else:
+                delimstart = SentenceStartPos
+            Morphemes.append(string[delimstart:i+1])
+            #keep track of everything
+            pairdelimiterpos["AllList"].append(i)
+            #NEW
+            #adding this line since words "stuck" next to punctuation get lost:
+            #Morphemes.append(string[pairdelimiterpos["AllList"][-1]:i+1])
+            
+            SentenceStartPos = i+1
+            SentenceStartPosList.append(i+1)
+        i += 1
+    #Morphemes
+    return Morphemes
+
 
 while True:
     inputtext = str(input("Exit or logout to leave \n"))
@@ -145,3 +400,11 @@ while True:
         #print("basislist", basislist)
         #print("fml", Address(basislist,M_(inputtext)))
         print(AutoVision(Address(basislist,M_(inputtext)),1))
+        #test
+        #f2 = [[4,5]]
+        #f1 = [[3,1],['g','t'],['r','y'],['y',"art"],[7,7],[90,9],[0,9],['o','o']]
+        #print(f1)
+        #print(f2)
+        #print(Compose(f1,f2))
+        #testing
+        print(getsourcelines(M_))
