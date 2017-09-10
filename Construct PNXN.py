c34ba8ce773f257c1d6744ecc5d93c41e51aaede
RPC <groditz@gmail.com>
2017-09-10 16:07:16 -0700

    uh just run this on linux instead of on windows cmd

--- Construct PNXN.py
+++ Construct PNXN.py
@@ -1,11 +1,12 @@
-from casual import *
+from MiraExternals import *
 
 '''
 idea:
 just fucking make P^N(NxN) the stupid way
 problems:
-how to start from where you left off?
 what is the data format?
+how to start from where you left off?
+
 
 idea:
 just run every hour x
