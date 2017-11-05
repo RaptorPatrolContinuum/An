ab758bc4ca40833892f0431ad668afd96b51cd11
RPC <groditz@gmail.com>
2017-11-05 09:23:16 -0800

    commit changes or stash them before you merge

--- RPILinuxVersion/SITest.py
+++ RPILinuxVersion/SITest.py
@@ -1,5 +1,29 @@
 from MiraExternals import *
 
-#print("testing shittySI",ShittySI([[[['0','0'],['0','0']],[]],"Auto"]))
-#print("testing shittySI",ShittySI([[[['2','0']],[['1','0']]],"Auto"]))
-print("testing shittySI",ShittySI([[[['1', '0'], ['2', '0'], ['2', '1']], [['1', '0'], ['0', '1'], ['1', '1'], ['0', '2'], ['2', '1']]],"Auto"]))
+#print("testing shittySI",ShittySI([[[['0','0'],['1','0']],[]],"Auto"]))
+
+
+def IsAuto(E_G):
+    '''
+    checks if a graph is just on integers or not (so we can figure out to use autovision or not)
+    '''
+    for x in Vertex_(E_G):
+        try:
+            int(x)
+        except ValueError:
+            return False
+        return True
+print(IsAuto([['0','0'],['1','0']]))
+
+def IsAuto(E_G):
+    '''
+    checks if a graph is just on integers or not (so we can figure out to use auto functions or not)
+    '''
+    for x in Vertex_(E_G):
+        print("what is x?",x)
+        try:
+            int(x)
+        except ValueError:
+            return False
+            break
+    return True
