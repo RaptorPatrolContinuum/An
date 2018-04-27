2f4f4f845a2f579c3b9b9f3b777b94ea6ce44881
RPC <groditz@gmail.com>
2018-04-27 02:04:51 -0700

    actually gonna sleep now

---
+++ MIRA/EDITING INPUTTEXT TO WORK WITH COMPOSEMETA.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.6.3
+from MiraExternals import *
+
+while True:
+    inputtext = str(input("exit or logout to leave \n"))
+    if inputtext == "exit" or inputtext == "logout":
+        raise SystemExit
+        break
+    else:
+        print("what is OG inputtext?",inputtext)
+        #YOU HAVE TO MODIFY INPUTTEXT TO ESCAPE ALL THE FUCKING " FOR COMPOSEMETA TO WORK PROPERLY ON print("X")
+        #MAYBE NOT, I NEEED TO FUCKING SLEEP
+        inputtext = inputtext.replace("\"", '\\'+"\"")
+        print("inputtext check",inputtext)
+        try:
+            eval(str(inputtext))
+        except Exception as e:
+            print("wtf is wrong",e)
